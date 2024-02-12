2754eced0ee1f1da24af91c864f5c5b60da10ba8
kevindheeraj <77147701+kevindheeraj@users.noreply.github.com>
2024-02-12 15:38:56 +0530

    Update testecom.py

--- CaseStudies/case study - Ecommerce application/main/testecom.py
+++ CaseStudies/case study - Ecommerce application/main/testecom.py
@@ -6,24 +6,25 @@ from assignments.ecommer.entity.product import Product
 
 class TestOrderProcessorRepositoryImpl(unittest.TestCase):
 
-    @patch('builtins.input', side_effect=['22', '18', '33', '2'])
+    @patch('builtins.input', side_effect=['19', '33', '2'])
     def test_add_to_cart_success(self, mock_input):
         order_processor_repository = OrderProcessorRepositoryImpl()
         # Add the product to the cart
         result = order_processor_repository.add_to_cart()
         self.assertTrue(result)
 
-    @patch('builtins.input', side_effect=['14', '123 Main St, City'])
+    @patch('builtins.input', side_effect=['19', '123 Main St, City'])
     def test_place_order_success(self, mock_input):
         order_processor_repository = OrderProcessorRepositoryImpl()
         result = order_processor_repository.place_order()
         self.assertTrue(result)
 
-    @patch('builtins.input', side_effect=['42', 'plates', '55.50', 'Heavy steel', '10'])
+    @patch('builtins.input', side_effect=['bars', '555.50', 'Heavy steel', '10'])
     def test_create_product_success(self, mock_input):
         order_processor_repository = OrderProcessorRepositoryImpl()
         result = order_processor_repository.create_product()
         self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()
